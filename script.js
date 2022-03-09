90a747ca574d5455b2077bd27dcd4bdd74c903d9
Liam Hornschild-Bear <liamop03@icloud.com>
2022-03-09 03:19:19 -0500

    master

--- script.js
+++ script.js
@@ -14,40 +14,29 @@ if (currentTime.getMonth() == 2 && currentTime.getDate() == 9) {
 }
 }
 }
-
-
-
-//Show current value of url as value currentURL.
 if(window.location.href.indexOf("options") > -1) {
     chrome.storage.sync.get('url', function(result) {
         document.getElementById("currentURL").innerHTML = result.url;
     });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
     var link = document.getElementById('submit');
-    // onClick's logic below:
     link.addEventListener('click', function() {
         chrome.storage.sync.get('url', function(result) {
             var url = document.getElementById('url').value;
             chrome.storage.sync.set({
                 url: url
             }, function(result) {
-                console.log("saved")
-                console.log(result);
             });
             document.getElementById("currentURL").innerHTML = url;
         });
     });
 });
-
 chrome.storage.sync.get('url', function(result) {
     if(result.url == undefined) {
         chrome.storage.sync.set({
             url: 'https://www.youtube.com/watch?v=a3Z7zEc7AXQ'
         }, function(result) {
-            console.log("saved")
-            console.log(result);
         });
     }
 });
